8fbb25ab0f203701eb5af0cd56612f1445fd78b1
Michael Stapelberg <michael@stapelberg.de>
2017-09-29 09:47:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+next-chrome-for-i3 focuses the chrome window on the current workspace or starts a new chrome instance.
+
+## Installation
+
+If you haven’t already, [install Go](https://golang.org/dl/).
+
+```shell
+go get -u github.com/stapelberg/next-chrome-for-i3
+```
+
+## Usage
+
+```shell
+next-chrome-for-i3
+```
---
+++ nextchrome.go
@@ -0,0 +1,37 @@
+// Program next-chrome-for-i3 focuses the chrome window on the current
+// workspace or starts a new chrome instance.
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"go.i3wm.org/i3"
+)
+
+func logic() error {
+	tree, err := i3.GetTree()
+	if err != nil {
+		return err
+	}
+
+	ws := tree.Root.FindFocused(func(n *i3.Node) bool { return n.Type == i3.WorkspaceNode })
+	if ws == nil {
+		return fmt.Errorf("could not locate workspace")
+	}
+
+	if chrome := ws.FindChild(func(n *i3.Node) bool { return strings.HasSuffix(n.Name, "- Google Chrome") }); chrome != nil {
+		_, err = i3.RunCommand(fmt.Sprintf(`[con_id="%d"] focus`, chrome.ID))
+	} else {
+		_, err = i3.RunCommand(`exec google-chrome`)
+	}
+
+	return err
+}
+
+func main() {
+	if err := logic(); err != nil {
+		log.Fatal(err)
+	}
+}
